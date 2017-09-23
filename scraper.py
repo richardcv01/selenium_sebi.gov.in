e27d8355ae9724e41430ec00b03e34d037220e66
richardcv <richardv@ukr.net>
2017-09-24 01:19:47 +0300

    finish

--- scraper.py
+++ scraper.py
@@ -10,62 +10,90 @@ BASE_URL = 'http://www.sebi.gov.in/sebiweb/other/OtherAction.do?doRecognisedFpi=
 phantomjs_path = os.path.abspath("G:\phantomjs\\bin\phantomjs.exe")
 print(phantomjs_path)
 
+def cler_file(file_name):
+    with open(file_name, 'r') as f_input, open('data2.csv', 'w',newline='') as f_output:
+        csv_output = csv.writer(f_output, delimiter = '\t')
+        n = 0
+        for row in csv.reader(f_input, delimiter = '\t'):
+            n = n + 1
+            if len(row):
+
+
+               if row[0][0:4] != 'Name' or n == 1:
+                   csv_output.writerow(row)
+
 def write_svc(data):
-    FILENAME = "users.csv"
-    with io.open(FILENAME, "a", encoding="utf-8") as file:
+    FILENAME = "Data.csv"
+    with io.open(FILENAME, "a", encoding="utf-8", newline='') as file:
         #f.write(html)
     #with open(FILENAME, "w", newline="") as file:
-        columns = list(data[0].keys())
+        #columns = list(data[0].keys())
+        columns = ['Name', 'Registration No', 'E-mail', 'Telephone', 'Fax No', 'Address', 'Validity',
+                   'Exchange Name', 'Affiliated Broke', 'Affiliated Broker Reg. No']
         writer = csv.DictWriter(file, fieldnames=columns)
         writer.writeheader()
         # запись нескольких строк
         writer.writerows(data)
         print("write Ok!")
 
-def parse_url(html):
-    tree = etree.HTML(html)
-    name_list = tree.xpath('//div[@class="value varun-text"]/span/text()')
+def replace(st):
+    return re.sub("^\s+|\n|\r|\s+$", '',st)
 
-    #name_val = tree.xpath('//div[@class="value"]/span/text()')
+def parse_url(html, n):
+    tree = etree.HTML(html)
     list_element = tree.xpath('//div[@class="fixed-table-body card-table"]')
-    name = tree.xpath('.//div[@class="value varun-text"]/span/text()')
-    el = list_element[0].xpath('.//div[@class="value"]/span/text()')
+    #name = tree.xpath('.//div[@class="value varun-text"]/span/text()')
+    #el = list_element[0].xpath('.//div[@class="value"]/span/text()')
     list_data = []
+
     for element in list_element:
         table = element.xpath('.//div[@class="value"]/span/text()')
         dic = {}
-        dic["name"] = element.xpath('.//div[@class="value varun-text"]/span/text()')[0]
-        if len(table) == 6:
-            dic['Registration No'] =  element.xpath('.//div[@class="value"]/span/text()')[0]
-            dic['Telephone'] = "",
-            dic['Fax No'] = "",
-            dic['Address'] = element.xpath('.//div[@class="value"]/span/text()')[1]
-            dic['Validity'] = element.xpath('.//div[@class="value"]/span/text()')[2]
-            dic['Exchange Name'] = element.xpath('.//div[@class="value"]/span/text()')[3]
-            dic['Affiliated Broke'] = element.xpath('.//div[@class="value"]/span/text()')[4]
-            dic['Affiliated Broker Reg. No'] = element.xpath('.//div[@class="value"]/span/text()')[5]
-        elif len(table) == 7:
-            dic['Registration No'] = element.xpath('.//div[@class="value"]/span/text()')[0]
-            dic['Telephone'] = element.xpath('.//div[@class="value"]/span/text()')[1]
-            dic['Fax No'] = ""
-            dic['Address'] = element.xpath('.//div[@class="value"]/span/text()')[2]
-            dic['Validity'] = element.xpath('.//div[@class="value"]/span/text()')[3]
-            dic['Exchange Name'] = element.xpath('.//div[@class="value"]/span/text()')[4]
-            dic['Affiliated Broke'] = element.xpath('.//div[@class="value"]/span/text()')[5]
-            dic['Affiliated Broker Reg. No'] = element.xpath('.//div[@class="value"]/span/text()')[6]
-        elif len(table) == 8:
-            dic['Registration No'] = element.xpath('.//div[@class="value"]/span/text()')[0]
-            dic['Telephone'] = element.xpath('.//div[@class="value"]/span/text()')[1]
-            dic['Fax No'] = element.xpath('.//div[@class="value"]/span/text()')[2]
-            dic['Address'] = element.xpath('.//div[@class="value"]/span/text()')[3]
-            dic['Validity'] = element.xpath('.//div[@class="value"]/span/text()')[4]
-            dic['Exchange Name'] = element.xpath('.//div[@class="value"]/span/text()')[5]
-            dic['Affiliated Broke'] = element.xpath('.//div[@class="value"]/span/text()')[6]
-            dic['Affiliated Broker Reg. No'] = element.xpath('.//div[@class="value"]/span/text()')[7]
+        dic["Name"] = replace(element.xpath('.//div[@class="value varun-text"]/span/text()')[0])
+        try:
+            i = 0
+            el_email = replace(element.xpath('.//div[@class="value"]/span/text()')[1])
+            el_fax = replace(element.xpath('.//div[@class="value"]/span/text()')[2])
+            if re.search('@', el_email):
+                dic['E-mail'] = el_email
+                i = i + 1
+                el_fax = ""
+            else:
+                dic['E-mail'] = ""
+
+            if len(table) == 6:
+                dic['Registration No'] =  replace(element.xpath('.//div[@class="value"]/span/text()')[0])
+                dic['Telephone'] = "",
+                dic['Fax No'] = "",
+                dic['Address'] = replace(element.xpath('.//div[@class="value"]/span/text()')[1])
+                dic['Validity'] = replace(element.xpath('.//div[@class="value"]/span/text()')[2])
+                dic['Exchange Name'] = replace(element.xpath('.//div[@class="value"]/span/text()')[3])
+                dic['Affiliated Broke'] = replace(element.xpath('.//div[@class="value"]/span/text()')[4])
+                dic['Affiliated Broker Reg. No'] = replace(element.xpath('.//div[@class="value"]/span/text()')[5])
+            elif len(table) == 7:
+                dic['Registration No'] = replace(element.xpath('.//div[@class="value"]/span/text()')[0])
+                dic['Telephone'] = replace(element.xpath('.//div[@class="value"]/span/text()')[1])
+                dic['Fax No'] = ""
+                dic['Address'] = replace(element.xpath('.//div[@class="value"]/span/text()')[2])
+                dic['Validity'] = replace(element.xpath('.//div[@class="value"]/span/text()')[3])
+                dic['Exchange Name'] = replace(element.xpath('.//div[@class="value"]/span/text()')[4])
+                dic['Affiliated Broke'] = replace(element.xpath('.//div[@class="value"]/span/text()')[5])
+                dic['Affiliated Broker Reg. No'] = replace(element.xpath('.//div[@class="value"]/span/text()')[6])
+            elif len(table) == 8:
+                dic['Registration No'] = replace(element.xpath('.//div[@class="value"]/span/text()')[0])
+                dic['Telephone'] = replace(element.xpath('.//div[@class="value"]/span/text()')[1+i])
+                dic['Fax No'] = el_fax
+                dic['Address'] = replace(element.xpath('.//div[@class="value"]/span/text()')[3])
+                dic['Validity'] = replace(element.xpath('.//div[@class="value"]/span/text()')[4])
+                dic['Exchange Name'] = replace(element.xpath('.//div[@class="value"]/span/text()')[5])
+                dic['Affiliated Broke'] = replace(element.xpath('.//div[@class="value"]/span/text()')[6])
+                dic['Affiliated Broker Reg. No'] = replace(element.xpath('.//div[@class="value"]/span/text()')[7])
+        except IndexError as e:
+            print('line=', n ,'...', e)
         list_data.append(dic)
+        #print(dic)
     write_svc(list_data)
-    print(list_data)
-    return list_element
+    #return list_element
 
 def next_page_link(driver, number_page):
     jscode = "javascript: searchFormFpi('n', '" + str(number_page) + "');"
@@ -91,8 +119,8 @@ class Web_Driver():
             if new_height == last_height:
                 break
             last_height = new_height
-        html = driver.page_source
-        parse_url(html)
+        #html = driver.page_source
+        #parse_url(html)
         return driver
 
 
@@ -101,12 +129,14 @@ def get_htmlN(n,N,driver):
     N_ = N
     driver_ = driver
     next_page_link(driver, n_)
-    time.sleep(2)
+    time.sleep(1)
     html = driver.page_source
+    #time.sleep(3)
+    parse_url(html, n_)
     n_=n_+1
     if n < N:
         get_htmlN(n_, N_, driver_)
-    print('parse', n, parse_url(html))
+
 
 def get_count_page(driver):
     element_li = driver.find_element_by_xpath('//li[@class=""]/a')
@@ -116,25 +146,61 @@ def get_count_page(driver):
 
 from threading import Thread
 def main():
+
     time1 = time.time()
     driver1 = Web_Driver().get_html()
-    #driver2 = Web_Driver().get_html()
-    #driver3 = Web_Driver().get_html()
-    #driver4 = Web_Driver().get_html()
-    #get_htmlN(1, 400, driver1)
-    #t1 = Thread(target=get_htmlN, args=(0,0, driver1))
-    #t2 = Thread(target=get_htmlN, args=(11,20, driver2))
-    #t3 = Thread(target=get_htmlN, args=(201, 300, driver3))
-    #t4 = Thread(target=get_htmlN, args=(301, 400, driver4))
-    #t1.start()
-    #t2.start()
-    #t3.start()
-    #t4.start()
-    #t1.join()
-    #t2.join()
-    #t3.join()
-    #t4.join()
+    #get_htmlN(0,0,driver1)
+    coun = get_count_page(driver1)
+    print(coun)
+    driver2 = Web_Driver().get_html()
+    driver3 = Web_Driver().get_html()
+    driver4 = Web_Driver().get_html()
+    driver5 = Web_Driver().get_html()
+    driver6 = Web_Driver().get_html()
+    driver7 = Web_Driver().get_html()
+    driver8 = Web_Driver().get_html()
+    driver9 = Web_Driver().get_html()
+    driver10 = Web_Driver().get_html()
+    driver11 = Web_Driver().get_html()
+    #et_htmlN(1, 400, driver1)
+    t1 = Thread(target=get_htmlN, args=(0,100, driver1))
+    t2 = Thread(target=get_htmlN, args=(101,200, driver2))
+    t3 = Thread(target=get_htmlN, args=(201, 300, driver3))
+    t4 = Thread(target=get_htmlN, args=(301, 400, driver4))
+    t5 = Thread(target=get_htmlN, args=(401, 500, driver5))
+    t6 = Thread(target=get_htmlN, args=(501, 600, driver6))
+    t7 = Thread(target=get_htmlN, args=(601, 700, driver7))
+    t8 = Thread(target=get_htmlN, args=(701, 800, driver8))
+    t9 = Thread(target=get_htmlN, args=(801, 900, driver9))
+    t10 = Thread(target=get_htmlN, args=(901, 1000, driver10))
+    t11 = Thread(target=get_htmlN, args=(1001, coun, driver11))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+    t8.start()
+    t9.start()
+    t10.start()
+    t11.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    t6.join()
+    t7.join()
+    t8.join()
+    t9.join()
+    t10.join()
+    t11.join()
+
     time2 = time.time()
+    cler_file("Data.csv")
     print(time2 - time1)
 
 
